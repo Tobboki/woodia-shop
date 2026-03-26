8a1d57684ed7497a86f04946ae0855baa9549871
Yaser M. Shabana <yaser.m.shabana@gmail.com>
2026-03-26 02:15:48 +0200

    feat: create shared lib

---
+++ src/app/shared/components/select/index.ts
@@ -0,0 +1,4 @@
+export * from 'src/app/shared/components/select/select.component';
+export * from 'src/app/shared/components/select/select-item.component';
+export * from 'src/app/shared/components/select/select.imports';
+export * from 'src/app/shared/components/select/select.variants';
---
+++ src/app/shared/components/select/select.imports.ts
@@ -0,0 +1,4 @@
+import { ZardSelectItemComponent } from 'src/app/shared/components/select/select-item.component';
+import { ZardSelectComponent } from 'src/app/shared/components/select/select.component';
+
+export const ZardSelectImports = [ZardSelectComponent, ZardSelectItemComponent] as const;
